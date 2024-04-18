ad2f16323298ec5b5367d57d6923c7cdcb3c7d41
pnl777 <hiseni1324@eryod.com>
2024-04-18 14:21:34 +0800

    Database and Backend setup handshake

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import dbConnect from "./config/db.js";
 
 // Configurations
 dotenv.config();
@@ -15,5 +16,6 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 app.listen(PORT, async () => {
+  await dbConnect();
   console.log(`Application is running on port: ${PORT}`);
 });
